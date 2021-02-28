09dcee1f769655700307c2b2c9b2f4f13e39a1cf
Ilias Antonopoulos <ilias.antonopoulos@yahoo.gr>
2021-02-28 02:05:50 +0200

    [feat]: add primary key on labels

--- sql/labels_constraints.sql
+++ sql/labels_constraints.sql
@@ -1,2 +1,3 @@
+ALTER TABLE labels ADD PRIMARY KEY (left_camera_id, right_camera_id);
 ALTER TABLE labels ADD CONSTRAINT "fk_cameras_id_1" FOREIGN KEY (left_camera_id) REFERENCES cameras (id);
 ALTER TABLE labels ADD CONSTRAINT "fk_cameras_id_2" FOREIGN KEY (right_camera_id) REFERENCES cameras (id);
